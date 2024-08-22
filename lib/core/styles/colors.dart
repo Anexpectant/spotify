53ce21974a0e3cfd9c86578712e1c73a1d7c35c3
anexpectant <anexpectant@anexpectant.ir>
2024-08-22 16:19:34 +0330

    #feat: adding primary colors

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const _defaultPrimaryColor = 0xFF955ce6;
+const darkPrimaryColor = Color(0xFF773EC8);
+const primaryColor = MaterialColor(_defaultPrimaryColor, <int, Color>{
+  50: Color(0xfff1e9fc),
+  100: Color(0xffd4bdf5),
+  400: Color(0xffb790ee),
+  600: Color(0xFF9a64e7),
+  800: Color(0xff7e38e0),
+  900: Color(0xff641fc7),
+});
