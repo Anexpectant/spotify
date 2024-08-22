d0b795dd829b54438cb8adf9833f58bb71fa3d60
anexpectant <anexpectant@anexpectant.ir>
2024-08-22 16:30:10 +0330

    #refactor: colors file

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-const _defaultPrimaryColor = 0xFF955ce6;
-const darkPrimaryColor = Color(0xFF773EC8);
-const primaryColor = MaterialColor(_defaultPrimaryColor, <int, Color>{
-  50: Color(0xfff1e9fc),
-  100: Color(0xffd4bdf5),
-  400: Color(0xffb790ee),
-  600: Color(0xFF9a64e7),
-  800: Color(0xff7e38e0),
-  900: Color(0xff641fc7),
+// primary colors
+const primaryColor = Color(0xFF42c83c);
+
+// grey colors
+const _defaultGreyColor = 0xFFcccccc;
+const greyColor = MaterialColor(_defaultGreyColor, <int, Color>{
+  200: Color(0xFFf4f6fa),
+  400: Color(0xFFedf1f7),
+  600: Color(0xFFe7ecf4),
+  800: Color(0xFFe3e9f2),
+  900: Color(0xffc5cbce),
 });
+const darkGrey = Color(0xFF343434);
+
+// backGround colors
+const lightBackground = Color(0xFFf2f2f2);
+const darkBackground = Color(0xFF0d0c0c);
