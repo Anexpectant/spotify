63727183e2f4f2857892fc445f86878517295c76
anexpectant <anexpectant@anexpectant.ir>
2024-08-24 18:08:57 +0330

    #feat: adding font size and spacings size

---
+++ lib/core/constants/numbers/spacings.dart
@@ -0,0 +1,65 @@
+class Spacings {
+  // padding
+  static const double paddingXXs = 2;
+  static const double paddingXs = 4;
+  static const double paddingHSm = 8;
+  static const double paddingSm = 12;
+  static const double paddingMd = 16;
+  static const double paddingLg = 20;
+  static const double paddingXl = 24;
+  static const double paddingXXl = 32;
+  static const double padding3Xl = 35;
+  static const double padding4Xl = 40;
+  static const double padding5Xl = 48;
+  static const double padding6Xl = 60;
+  static const double padding7Xl = 72;
+  static const double padding8Xl = 80;
+
+  // margin
+  static const double marginXXs = 2;
+  static const double marginXs = 4;
+  static const double marginSm = 8;
+  static const double marginLSm = 12;
+  static const double marginMd = 16;
+  static const double marginLg = 20;
+  static const double marginXl = 24;
+  static const double marginXXl = 32;
+  static const double margin3Xl = 35;
+  static const double margin4Xl = 40;
+  static const double margin5Xl = 48;
+  static const double margin6Xl = 64;
+  static const double margin7Xl = 72;
+  static const double margin8Xl = 76;
+  static const double margin15Xl = 130;
+  static const double margin16Xl = 238;
+
+  //radius
+  static const double radiusXs = 2;
+  static const double radiusSm = 4;
+  static const double radiusMd = 8;
+  static const double radiusLg = 12;
+  static const double radiusXl = 16;
+  static const double radiusXXl = 20;
+  static const double radius3Xl = 24;
+  static const double radius4Xl = 30;
+  static const double radius6Xl = 40;
+  static const double radiusCircle = 100;
+
+  //size
+  static const double nanoSize = 4;
+  static const double microSize = 8;
+  static const double size5Xs = 12;
+  static const double size4Xs = 16;
+  static const double size3Xs = 24;
+  static const double sizeXXs = 32;
+  static const double sizeXs = 48;
+  static const double sizeSm = 54;
+  static const double sizeMd = 64;
+  static const double sizeLg = 86;
+  static const double sizeXl = 110;
+  static const double sizeXXl = 120;
+  static const double size3Xl = 160;
+  static const double size4Xl = 240;
+  static const double maxWidth = 600;
+  static const double sizeMaxPageWidth = 1000;
+}
