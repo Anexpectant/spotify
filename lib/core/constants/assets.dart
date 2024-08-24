dcedb830c3babc04e6a77e8953af324d59a8f54d
anexpectant <anexpectant@anexpectant.ir>
2024-08-24 18:26:27 +0330

    #feat: adding assets folder and files

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,22 @@
+class Assets {
+  static const String BASE = "assets/";
+  static const String SVG = ".svg";
+  static const String PNG = ".png";
+  static const String JSON = ".json";
+
+  // Directories
+  static const String ANIMATION = "animation/";
+  static const String ICONS = "icons/";
+  static const String PLACEHOLDER = "placeHolder/";
+  static const String PICTURES = "picture/";
+
+  // Animation-Directory  
+
+  // Icons-Directory
+
+  // Placeholders-Directory
+  
+  // Pictures-Directory
+  
+}
+
